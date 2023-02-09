57be929af293e84cf2bd6c593056487342b9299e
calihuddleston <calihuddleston@gmail.com>
2023-02-08 23:20:45 -0500

    uploading files/documents and tested with cheap navbar

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#menu">Menu</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+            <li>
+              <a href="#order">Order</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
